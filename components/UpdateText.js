fea337759e4c333af4a7773d52e13516300594e7
Ben Ipsen <ben.ipsen@bumped.com>
2021-12-27 14:29:48 -0800

    cleaner envelope, font treatments

--- components/UpdateText.js
+++ components/UpdateText.js
@@ -3,7 +3,7 @@ import styles from '../styles/MainLayout.module.css';
 
 export const UpdateText = ({ title, published, summary, description }) => (
     <div className={styles.update}>
-     {published ? <h4>{published}</h4> : null}
+     {published ? <h4>{new Date(published).toLocaleDateString()}</h4> : null}
      {title ? <h1>{title}</h1> : null}
      {summary ? <h2>{summary}</h2> : null}
      {description && renderRichText(description)}
