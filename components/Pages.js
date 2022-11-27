e60a12850a5ff0119d0eb535ad2028d90dc29a4c
Dusty McCord <Dustymccord@gmail.com>
2022-11-26 23:18:06 -0800

    Update internal links to use next router

--- components/Pages.js
+++ components/Pages.js
@@ -1,19 +1,19 @@
+import Link from "next/link"
 
-
-export const Pages = ({ items, ulStyle, display }) => display 
-    ? (
-        <ul className={ulStyle}>
-            {items.map(item => (
-                <li key={item.url} className="menu-item">
-                    <a href={`/${item.url}`} className="menu-link">
-                        <span className="menu-link-text">{item.title}</span>
-                    </a>
-                    {item.children.length > 0 
-                        ? <Pages items={item.children} ulStyle={{}} display={true} /> 
-                        : null
-                    }
-                </li>
-            ))}
-        </ul> 
-    )
-    : null
+export const Pages = ({ items, ulStyle, display }) =>
+  display ? (
+    <ul className={ulStyle}>
+      {items.map((item) => (
+        <li key={item.url} className="menu-item">
+          <Link href={`/${item.url}`}>
+            <a className="menu-link">
+              <span className="menu-link-text">{item.title}</span>
+            </a>
+          </Link>
+          {item.children.length > 0 ? (
+            <Pages items={item.children} ulStyle={{}} display={true} />
+          ) : null}
+        </li>
+      ))}
+    </ul>
+  ) : null
